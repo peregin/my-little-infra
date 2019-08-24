b965b54d7085fb61defa831c860a5beeba6c63be
levi <velocorner.com@gmail.com>
2019-08-24 15:21:44 +0200

    add a web server template on EC2

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# DEPLOY A SINGLE EC2 INSTANCE
+# This template runs a web server on a single EC2 Instance
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ----------------------------------------------------------------------------------------------------------------------
+# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
+# This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
+# ----------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  required_version = ">= 0.12"
+}
+
 # configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
@@ -8,11 +22,28 @@ resource "aws_instance" "web" {
   # AMI ID for Centos 7 x86_64 https://wiki.centos.org/Cloud/AWS
   # version 1801_01
   ami = "ami-4bf3d731"
-
   instance_type = "t2.micro"
   key_name      = "aws-peregin"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-  tags {
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+  tags = {
     Name = "web-infra"
   }
 }
+
+resource "aws_security_group" "instance" {
+  name = "web-instance"
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
