d483edf25e63971c31151f0a103b24fc17780038
levi <velocorner.com@gmail.com>
2019-09-18 22:32:30 +0200

    setup default parameters

--- machine.sh
+++ machine.sh
@@ -3,21 +3,22 @@
 ENVIRONMENT=$(echo "$1" | awk '{print tolower($0)}')
 ACTION=$(echo "$2" | awk '{print tolower($0)}')
 if [ -z "$ACTION" ]; then
-  ACTION="create"
+  if [ "$ENVIRONMENT" == "local" ]; then
+    ACTION="up"
+  else
+    ACTION="create"
+  fi
 fi
+echo "environment[$ENVIRONMENT] action[$ACTION]"
 
 function printUsage() {
   echo $"Usage: $(basename "$0") {local|aws} [create|destroy]"
 }
 
-case "$ACTION" in
-  "create"|"destroy")
-    ;;
-  *)
-    printUsage
-    exit 1
-    ;;
-esac
+function setupWithVagrant() {
+  cd machine/local
+  cd ../..
+}
 
 case "$ENVIRONMENT" in
   "local")
@@ -31,5 +32,3 @@ case "$ENVIRONMENT" in
     exit 1
     ;;
 esac
-
-echo "environment is $ENVIRONMENT"
--- provision.sh
+++ provision.sh
@@ -3,20 +3,17 @@
 ENVIRONMENT=$(echo "$1" | awk '{print tolower($0)}')
 
 function printUsage() {
-  echo $"Usage: $(basename "$0") {local|aws} [create|destroy]"
+  echo $"Usage: $(basename "$0") {local|aws}"
 }
 
+echo "environment[$ENVIRONMENT]"
+
 case "$ENVIRONMENT" in
-  "local")
-    setupWithVagrant $ENVIRONMENT $ACTION
-    ;;
-  "aws")
-    setupWithTerraform $ENVIRONMENT $ACTION
+  "local|aws")
+    setupWithAnsible $ENVIRONMENT
     ;;
   *)
     printUsage
     exit 1
     ;;
 esac
-
-echo "environment is $ENVIRONMENT"
