e470d9c1845492d71f33b37a1f91dc5c6db1cf70
levi <velocorner.com@gmail.com>
2019-10-31 23:22:42 +0100

    dockerize web application

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,20 @@ if [ -z "$STACK" ]; then
   exit 1
 fi
 
+if [ -z "$ADMIN_PASSWORD" ]; then
+  echo "ADMIN_PASSWORD environment variable must be set"
+  exit 1
+fi
+
+if [ -z "$APP_CONFIG_DIR" ]; then
+  echo "APP_CONFIG_DIR environment variable must be set"
+  exit 1
+fi
+if [ ! -d "$APP_CONFIG_DIR" ]; then
+  echo "[$APP_CONFIG_DIR] directory does not exists"
+  exit 1
+fi
+
 case "$ENVIRONMENT" in
   "local"|"aws")
     echo "deploying to environment[$ENVIRONMENT]"
