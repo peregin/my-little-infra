6fe109b37fe301b26df345572eaab798e55d624e
Levente Fall <levente.fall@gmail.com>
2021-02-20 12:22:40 +0100

    update portainer version, notes

--- machine.sh
+++ machine.sh
@@ -12,7 +12,7 @@ if [ -z "$COMMAND" ]; then
 fi
 
 function printUsage() {
-  echo $"Usage: $(basename "$0") <local|aws> [command]"
+  echo $"Usage: $(basename "$0") <local|aws> [up|create|destroy|ssh]"
 }
 
 function setupWithVagrant() {
