20563736f02e5264f8a99ce50d681e16aba8d237
levi <velocorner.com@gmail.com>
2023-02-14 22:28:37 +0100

    add peregin to ssl

--- provision/playbook/roles/nginx/files/aws-certificate.sh
+++ provision/playbook/roles/nginx/files/aws-certificate.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# eventually add peregin.com
 # for testing use --dry-run
 certbot -m velocorner.com@gmail.com \
  -d velocorner.com \
  -d www.velocorner.com \
  -d dev.velocorner.com \
  -d status.velocorner.com \
- -d marketing.velocorner.com \
  -d docker.velocorner.com \
- -d storage.velocorner.com \
  -d search.velocorner.com \
+ -d peregin.com \
+ -d www.peregin.com \
  --webroot -w /opt/letsencrypt certonly
