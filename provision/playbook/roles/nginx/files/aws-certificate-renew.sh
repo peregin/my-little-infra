4a6b60f42e1b895c7a89e80b3584fbdfe075993b
levi <velocorner.com@gmail.com>
2023-04-01 20:03:30 +0200

    list ssl certificates

--- provision/playbook/roles/nginx/files/aws-certificate-renew.sh
+++ provision/playbook/roles/nginx/files/aws-certificate-renew.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 certbot renew --pre-hook "systemctl stop nginx" --post-hook "systemctl start nginx"
+
+# remove old one
+# sudo certbot revoke --cert-name velocorner.com
+# list
+# sudo certbot certificates
