bcf73d258aa0aa52f45f62e93013fba09531672d
levi <velocorner.com@gmail.com>
2023-04-25 23:18:48 +0200

    use this to renew certificates

--- provision/playbook/roles/nginx/files/aws-certificate-renew.sh
+++ provision/playbook/roles/nginx/files/aws-certificate-renew.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# this doesn't work sometimes
 certbot renew --pre-hook "systemctl stop nginx" --post-hook "systemctl start nginx"
+
+# RENEW - use this !!!
+# sudo certbot renew
+# sudo systemctl restart nginx
 
 # remove old one
 # sudo certbot revoke --cert-name velocorner.com
