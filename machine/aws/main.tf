2fecda71666f518ee68dc0452ba64867d6ee92e7
levi <velocorner.com@gmail.com>
2024-04-19 23:12:14 +0200

    assign iam policy for s3 access

--- machine/aws/main.tf
+++ machine/aws/main.tf
@@ -28,6 +28,7 @@ resource "aws_instance" "web" {
     Name = "web-infra"
   }
   vpc_security_group_ids = [aws_security_group.instance.id]
+  iam_instance_profile   = aws_iam_instance_profile.web_instance_profile.id
 }
 
 resource "aws_security_group" "instance" {
@@ -83,4 +84,47 @@ resource "aws_s3_bucket_public_access_block" "user_data" {
   restrict_public_buckets = true
 }
 
+resource "aws_iam_role" "web_iam_role" {
+  name = "web_iam_role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement": [{
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_instance_profile" "web_instance_profile" {
+  name = "web_instance_profile"
+  role = aws_iam_role.web_iam_role.name
+}
+
+resource "aws_iam_role_policy" "web_iam_role_policy" {
+  name = "web_iam_role_policy"
+  role = aws_iam_role.web_iam_role.id
+  policy      = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": ["s3:ListBucket"],
+        "Resource": ["arn:aws:s3:::peregin-user-data"]
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:DeleteObject"
+        ],
+        "Resource": ["arn:aws:s3:::peregin-user-data/*"]
+      }
+    ]
+  })
+}
 
